ad5306c092e2822dbdf58261ed891805e5fff2f9
Mino <mino.s2000@outlook.com>
2018-08-27 11:27:32 +0900

    add md5sum script.

---
+++ Compute-MD5Hash/Compute-MD5Hash.ps1
@@ -0,0 +1,8 @@
+﻿param(
+    [Parameter(Mandatory)]
+    [string]$FilePath
+)
+
+$data = (New-Object IO.StreamReader $FilePath).BaseStream
+$md5 = [System.Security.Cryptography.MD5]::create()
+Write-Output [System.BitConverter]::ToString($md5.ComputeHash($data)).ToLower().Replace("-", "")

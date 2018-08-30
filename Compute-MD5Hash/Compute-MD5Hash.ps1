beb5a8b07e08bbc51638bf93cbf4b69f800c8806
Mino <mino.s2000@outlook.com>
2018-08-30 10:00:36 +0900

    deleted as a convenient cmdlet "Get-FileHash" appeared.

--- Compute-MD5Hash/Compute-MD5Hash.ps1
+++
@@ -1,8 +0,0 @@
-﻿param(
-    [Parameter(Mandatory)]
-    [string]$FilePath
-)
-
-$data = (New-Object IO.StreamReader $FilePath).BaseStream
-$md5 = [System.Security.Cryptography.MD5]::create()
-Write-Output [System.BitConverter]::ToString($md5.ComputeHash($data)).ToLower().Replace("-", "")
